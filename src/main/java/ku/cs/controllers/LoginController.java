21f197d4eaeeb78a558915adcbcf61a6f4010fde
Mekpearaphat <pearaphat.t@ku.th>
2021-09-02 23:11:02 +0700

    refactor how to

--- src/main/java/ku/cs/controllers/LoginController.java
+++ src/main/java/ku/cs/controllers/LoginController.java
@@ -106,7 +106,7 @@ public class LoginController {
 
     public void handleHowTo(ActionEvent event){
         try {
-            FXRouter.goTo("how_to_use");
+            FXRouter.goTo("how_to");
         } catch (IOException e) {
             System.err.println("ไปที่หน้า How To ไม่ได้");
             System.err.println("ให้ตรวจสอบการกำหนด route");
