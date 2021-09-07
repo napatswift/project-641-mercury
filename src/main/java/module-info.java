b570e5076a75c44adcfc0ab47f2ed307e4be7f47
Mekpearaphat <pearaphat.t@ku.th>
2021-09-07 23:52:54 +0700

    edit about us

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module ku.cs {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.junit.jupiter.api;
+    //requires org.junit.jupiter.api;
     requires com.opencsv;
 
     opens ku.cs to javafx.fxml;
