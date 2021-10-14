040fea8172b95e32f30bfe241fe1d788f746468b
napatswift <napat.do@ku.th>
2021-10-14 09:50:27 +0700

    Add animation

--- src/main/java/ku/cs/models/components/PromotionTicket.java
+++ src/main/java/ku/cs/models/components/PromotionTicket.java
@@ -1,21 +1,38 @@
 package ku.cs.models.components;
 
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Point3D;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.*;
+import javafx.util.Duration;
 
-public class PromotionTicket extends AnchorPane {
+public class PromotionTicket extends HBox {
     private final Label promotionInfoLabel;
     private final Label storeNameLabel;
     private final Label bigNumber;
     private final Label promotionDesc;
     private final Label percent;
+    private final StackPane leftStackPane;
+    private final StackPane rightStackPane;
+    private boolean toggle;
 
     public PromotionTicket(double width, double height, double radius, double leftRatio) {
         getStyleClass().add("promotion-ticket");
+        toggle = false;
+        promotionInfoLabel = new Label("Min Spend $" + 333);
+        storeNameLabel = new Label("Store Name");
+        bigNumber = new Label("$150");
+        promotionDesc = new Label("Up to");
+        percent = new Label("Off");
 
         Rectangle rectangle = new Rectangle(width * leftRatio, height);
         Circle circle = new Circle(width * leftRatio, 0, height * radius);
@@ -32,12 +49,6 @@ public class PromotionTicket extends AnchorPane {
         leftShape.getStyleClass().add("left-shape");
         rightShape.getStyleClass().add("right-shape");
 
-        promotionInfoLabel = new Label("Min Spend $" + 333);
-        storeNameLabel = new Label("Store Name");
-        bigNumber = new Label("$150");
-        promotionDesc = new Label("Up to");
-        percent = new Label("Off");
-
         bigNumber.getStyleClass().add("big-number");
         promotionInfoLabel.getStyleClass().add("promotion-info");
 
@@ -49,11 +60,40 @@ public class PromotionTicket extends AnchorPane {
         promotionInfo.setPrefWidth(width * (1 - leftRatio));
         promotionInfo.setAlignment(Pos.CENTER);
         promotionInfo.setPrefHeight(height);
-        HBox container = new HBox(shopInfo, promotionInfo);
-
-        AnchorPane.setLeftAnchor(rightShape, width * leftRatio);
-        getChildren().addAll(leftShape, rightShape, container);
+        leftStackPane = new StackPane(leftShape, shopInfo);
+        rightStackPane = new StackPane(rightShape, promotionInfo);
+        getChildren().addAll(leftStackPane, rightStackPane);
         setPrefWidth(-1);
+        setPadding(new Insets(5, 10, 5, 10));
+        setOnMouseEntered(this::toggleTicketAnimation);
+        setOnMouseExited(this::toggleTicketAnimation);
+    }
+
+    private void toggleTicketAnimation(MouseEvent mouseEvent){
+        TranslateTransition transition = new TranslateTransition(Duration.millis(200), rightStackPane);
+        RotateTransition rotateTransition = new RotateTransition(Duration.millis(200), rightStackPane);
+        if (toggle) {
+            rotateTransition.setFromAngle(10);
+            rotateTransition.setToAngle(0);
+
+            transition.setFromX(10);
+            transition.setToX(0);
+
+            transition.setFromY(10);
+            transition.setToY(0);
+        } else {
+            rotateTransition.setFromAngle(0);
+            rotateTransition.setToAngle(10);
+
+            transition.setFromY(0);
+            transition.setToY(10);
+
+            transition.setFromX(0);
+            transition.setToX(10);
+        }
+        rotateTransition.play();
+        transition.play();
+        toggle = !toggle;
     }
 
     public PromotionTicket() {
